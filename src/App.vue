2660b7fd6fa7d5bc21fbf2f9ed156ec78e09da06
roxxer42 <mpape234@gmail.com>
2022-02-11 21:46:15 +0100

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/roller">Roller</router-link> | 
-    <router-link to="/alarm">Alarm</router-link>
+     <b-button-group vertical>
+      <b-button><router-link to="/">Home</router-link></b-button>
+      <b-button><router-link to="/roller">Roller</router-link></b-button>
+      <b-button><router-link to="/alarm">Alarm</router-link></b-button>
+    </b-button-group>
   </div>
   <router-view/>
 </template>
 
 <style>
+body {margin:0}
 
+.nav {
+  width: 90px;
+  background-color: #303030;
+}
+
+.nav {
+  display: block;
+  text-align: center;
+  padding: 15px;
+  transition: all 0.5s ease;
+  color: white;
+  font-size: 35px;
+}
+
+.nav li:hover {
+  background-color: #006346;
+}
+
+.nav li.router-link-active {
+  background-color: #008c62;
+  color: white;
+}
 </style>
